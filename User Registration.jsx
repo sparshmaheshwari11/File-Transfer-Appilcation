7f99b7f67116273e90d3fae6d614e17fd4176f9f
sparshmaheshwari11 <124358561+sparshmaheshwari11@users.noreply.github.com>
2024-06-07 21:15:47 +0530

    Create User Registration.jsx

---
+++ User Registration.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async () => {
+        await axios.post('http://localhost:5000/register', { username, password });
+        alert('User registered');
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleRegister}>Register</button>
+        </div>
+    );
+};
+
+const Login = ({ onLogin }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        const response = await axios.post('http://localhost:5000/login', { username, password });
+        localStorage.setItem('token', response.data.token);
+        onLogin();
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
+        </div>
+    );
+};
+
+export { Register, Login };
